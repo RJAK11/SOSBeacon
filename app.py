90c31409822bbb80cd85a8e2fdff2030fdea033e
Rana Al-Khulaidi <69090627+RJAK11@users.noreply.github.com>
2024-03-10 17:07:26 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Client disconnected')
+
+if __name__ == '__main__':
+    socketio.run(app)
