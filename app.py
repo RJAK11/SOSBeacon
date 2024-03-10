5f352f30efc462a340ad403a2bea311a7627fa85
Rana <ralkhulaidi@gmail.com>
2024-03-10 17:50:20 -0400

    Fixed minor bugs

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, render_template
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
+
 @app.route("/")
 def home():
     return render_template('index.html')
 
+
 @socketio.on('connect')
 def handle_connect():
     print('Client connected')
+
 
 @socketio.on('disconnect')
 def handle_disconnect():
