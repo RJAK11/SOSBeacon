3988be37ac1293c9bc91b7956bd60e04df287ed7
Rana <ralkhulaidi@gmail.com>
2024-03-10 18:17:19 -0400

    Fixed bug in handling signal

--- app.py
+++ app.py
@@ -6,15 +6,22 @@ app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
 
-@app.route("/")
+@app.route('/')
 def home():
     return render_template('index.html')
 
+@app.route('/sos', methods=['POST'])
+def handle_sos_signal():
+    print("Recieved SOS Signal!")
+    socketio.emit('sos_recieved')
 
 @socketio.on('connect')
 def handle_connect():
     print('Client connected')
 
+@socketio.on('acknowledge_signal')
+def handle_acknowledge_signal():
+    print('SOS Signal Acknowledged!')
 
 @socketio.on('disconnect')
 def handle_disconnect():
