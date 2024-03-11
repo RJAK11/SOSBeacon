5e38429fd0ef5b06a23d718ad2ce63f9d4624e12
Rana <ralkhulaidi@gmail.com>
2024-03-11 01:28:25 -0400

    Fixed bug with page change

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 
 @app.route('/')
@@ -12,16 +12,18 @@ def home():
 
 @app.route('/sos', methods=['POST'])
 def handle_sos_signal():
-    print("Recieved SOS Signal!")
-    socketio.emit('sos_recieved')
+    print("Received SOS Signal!")
+    socketio.emit('sos_received')
+    return jsonify({'status': 'Success'}), 200
 
 @socketio.on('connect')
 def handle_connect():
     print('Client connected')
 
 @socketio.on('acknowledge_signal')
-def handle_acknowledge_signal():
+def handle_acknowledge_signal(data):
     print('SOS Signal Acknowledged!')
+    socketio.emit('acknowledge_signal', {'data': 'SOS Signal Acknowledged'})
 
 @socketio.on('disconnect')
 def handle_disconnect():
