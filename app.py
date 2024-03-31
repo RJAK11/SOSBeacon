717f9f0a827b80f76eb0e6f567bfd25b7f54e0e0
Rana Al-Khulaidi <69090627+RJAK11@users.noreply.github.com>
2024-03-30 20:59:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,33 +3,41 @@ from flask_socketio import SocketIO
 import threading
 import socket
 
+# Initialize Flask app
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-HOST = '10.88.111.9'
+# Configure TCP server host and port
+HOST = '10.88.111.20'
 PORT = 5000
 
-# https://realpython.com/python-sockets/
+# Run the TCP server
+# TCP server code was created with help from: https://realpython.com/python-sockets/
 def tcp_server():
 
     global connection
 
+    # Create socket
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        # Bind socket and listen
         s.bind((HOST, PORT))
         s.listen()
         print(f"TCP Server listening on {HOST}:{PORT}")
 
+        # Accept any conections 
         while True:
             conn, addr = s.accept()
             with conn:
                 connection = conn
 
+                # Wait to recieve data from the connection 
                 while True:
                     data = conn.recv(1024)
                     if not data:
                         break
                     message = data.decode('utf-8').strip()
+                    # Check message and emit the correct SOS signal using asynchronous execution
                     if message == "SOS: User Triggered":
                         socketio.start_background_task(target=emit_sos_received)
                     if message == "SOS: Temperature Triggered":
@@ -39,6 +47,7 @@ def tcp_server():
                     if message == "User cancelling SOS":
                         socketio.start_background_task(target=emit_sos_cancellation)
 
+# Emit the correct signal 
 def emit_sos_received():
     socketio.emit('sos_received', {'message': 'SOS signal received'})
     print("SOS: User Triggered")
@@ -55,6 +64,7 @@ def emit_sos_cancellation():
     socketio.emit('emit_sos_cancellation', {'message': 'SOS signal cancelled'})
     print("User cancelling SOS")
 
+# Route for the index page
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -67,6 +77,7 @@ def handle_connect():
 def handle_disconnect():
     print('Client disconnected')
 
+# Acknowledge the message
 @socketio.on('acknowledge_signal')
 def handle_acknowledge_signal(data):
     print('SOS Signal Acknowledged!')
@@ -77,6 +88,7 @@ def handle_acknowledge_signal(data):
         print('Acknowledgment sent to SOS Beacon.')
 
 if __name__ == '__main__':
+    # Create another thread to run the TCP server concurrently
     server_thread = threading.Thread(target=tcp_server, daemon=True)
     server_thread.start()
     socketio.run(app)
